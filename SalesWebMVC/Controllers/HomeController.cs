68b343337d0d9e62f3d71c0e19926d1bb370fe9a
Renato Vale <phal.aal@hotmail.com>
2023-05-26 12:44:19 -0300

    Preject created

---
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+using System.Diagnostics;
+
+namespace SalesWebMVC.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            ViewData["Massage"] = "Salles Web MVC App from C# Coursel";
+            ViewData["Dev"] = "Renato Vale Leal";
+            return View();
+           
+        }
+
+        public IActionResult Privacy() {
+            ViewData["Nome"] = "Marli Evangelista de Freiras";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
